50fac3d3e16e613df72a2aca51052cf7a96e798c
utftufutukgyftryidytftuv <overby999@gmail.com>
2025-04-10 22:07:50 +0300

    10.04.25 22:07

--- b/client.ts
+++
@@ -1,8 +0,0 @@
-import { build } from "bun";
-
-const a = await build({
-  // outdir: "./run/dist",
-  entrypoints: ["./b/server.ts"],
-});
-
-console.log("-----", "a", a);
--- src/files.ts
+++ src/files.ts
@@ -1,4 +1,5 @@
-import { appendFile } from "node:fs/promises";
+import { appendFile, stat } from "node:fs/promises";
+import { execCommand } from "./base.ts";
 
 const finalizeWithNewline = (str: string) => {
   if (str.at(-1) !== "\n") {
@@ -6,6 +7,18 @@ const finalizeWithNewline = (str: string) => {
   }
 
   return str;
+};
+
+const createDir = async (str: string) => {
+  const dirname = str.split("/").slice(0, -1).join("/");
+
+  const stats = await stat(dirname);
+
+  if (stats.isDirectory()) {
+    return;
+  }
+
+  await execCommand(`mkdir -p ${dirname}`);
 };
 
 const checkStrInFile = async (filePath: string, str: string) => {
@@ -27,6 +40,12 @@ const checkStrInFile = async (filePath: string, str: string) => {
 export const writeIfNew = async (filePath: string, str: string) => {
   if (await checkStrInFile(filePath, str)) {
     return;
+  }
+
+  await createDir(filePath);
+
+  if (!(await Bun.file(filePath).exists())) {
+    await execCommand(`touch ${filePath}`);
   }
 
   await appendFile(filePath, finalizeWithNewline(str));
