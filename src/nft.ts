09ea8f2f4dd333ab16783b0044f56b73fd495d07
utftufutukgyftryidytftuv <overby999@gmail.com>
2025-07-07 18:51:23 +0300

    07.07.25, 18:51

--- src/nft.ts
+++ src/nft.ts
@@ -2,7 +2,13 @@ import { execCommand } from "./base/base.ts";
 
 export const localNftChainName = "local_chain";
 
-const createNftTable = ({ allowedIpV4 }: { allowedIpV4: string[] }) => {
+const createNftTable = ({
+  allowedIpV4,
+  allowedPorts,
+}: {
+  allowedIpV4: string[];
+  allowedPorts: string[];
+}) => {
   const nftTable = `table inet ramm {
     set allowed_ipv4 {
       type ipv4_addr
@@ -16,8 +22,11 @@ const createNftTable = ({ allowedIpV4 }: { allowedIpV4: string[] }) => {
       ip saddr @allowed_ipv4 tcp dport 22 accept
       tcp dport 22 ct state new limit rate over 700/minute burst 5 packets drop
       tcp dport 22 accept
-      tcp dport 80 accept
-      tcp dport 443 accept
+      ${allowedPorts
+        .map((port) => {
+          return `tcp dport ${port} accept`;
+        })
+        .join("\n")}
     }
 
     chain local_chain {
@@ -31,13 +40,48 @@ const createNftTable = ({ allowedIpV4 }: { allowedIpV4: string[] }) => {
     }
   }
   `;
+  // const nftTable = `table inet ramm {
+  //   set allowed_ipv4 {
+  //     type ipv4_addr
+  //     flags dynamic
+  //     elements = { ${allowedIpV4.join("\n\t")} }
+  //   }
+
+  //   chain local_chain_base {
+  //     iif "lo" accept
+  //     ct state established,related accept
+  //     ip saddr @allowed_ipv4 tcp dport 22 accept
+  //     tcp dport 22 ct state new limit rate over 700/minute burst 5 packets drop
+  //     tcp dport 22 accept
+  //     tcp dport 80 accept
+  //     ${allowedPorts
+  //       .map((port) => {
+  //         return `tcp dport ${port} accept`;
+  //       })
+  //       .join("\n")}
+  //     tcp dport 443 accept
+  //   }
+
+  //   chain local_chain {
+  //     jump local_chain_base
+  //     drop
+  //   }
+
+  //   chain prerouting {
+  //     type filter hook prerouting priority mangle; policy accept;
+  //     fib daddr type local jump local_chain
+  //   }
+  // }
+  // `;
 
   return nftTable;
 };
 export const setupNftable = async ({
   allowedIpV4,
+  allowedPorts,
 }: {
   allowedIpV4: string[];
+  allowedPorts: string[];
 }) => {
   await execCommand("nft flush ruleset");
 
@@ -46,7 +90,7 @@ export const setupNftable = async ({
   // if (listTable.spawnResult.exitCode === 0) {
   //   await execCommand("nft delete table inet ramm");
   // }
-  const nftTable = createNftTable({ allowedIpV4 });
+  const nftTable = createNftTable({ allowedIpV4, allowedPorts });
   await execCommand(`nft -f - <<EOF\n${nftTable}\nEOF`);
 
   await execCommand("nft list ruleset > /etc/nftables.conf");
